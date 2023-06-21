9756b8e544f9dbd943b934377dd9108fae1ed15f
artem <core1024@gmail.com>
2023-06-21 16:11:13 +0300

    upd dependencies

--- ui/src/service/backendApiService.js
+++ ui/src/service/backendApiService.js
@@ -5,15 +5,15 @@ const API_URL = '/api/v1/'
 const API_IMAGES_URL = API_URL + 'sites/fishki/images/'
 const API_IMAGES_URL_SUFFIX = '/to/'
 
-class BackendApiService {
+const backendApiService = {
 
     getWebImagesOnPages(num1, num2) {
         return axios.get(API_IMAGES_URL + num1 + API_IMAGES_URL_SUFFIX + num2);
-    }
+    },
 
     saveAndPublishSelectedImages(images) {
         return axios.post(API_IMAGES_URL, images)
     }
 }
 
-export default new BackendApiService();
+export default backendApiService;
